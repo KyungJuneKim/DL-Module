f1106d7f9035c3e6b038de0c21e87e4817e71aa4
KyungJuneKim <kjune1999@naver.com>
2020-07-18 17:24:54 +0900

    third commit

--- Data.py
+++ Data.py
@@ -1,13 +1,28 @@
 from abc import *
+from random import randrange
+from typing import List
 
 
 class DataSet(metaclass=ABCMeta):
-    def __init__(self):
-        self.input_size = 100
-        self.output_size = 5
+    def __init__(self, input_size, output_size):
+        self.input_size = input_size
+        self.output_size = output_size
+
+        self.x_train = []
+        self.y_train = []
+        self.x_val = []
+        self.y_val = []
+        self.x_test = []
+        self.Y_test = []
+
+    def split_data(self, ratio: List[float] = None) -> List:
+        pass
+
+    def make_data(self):
+        pass
 
     @abstractmethod
-    def make_data(self):
+    def single_data(self, factor) -> List:
         pass
 
     @abstractmethod
@@ -16,8 +31,21 @@ class DataSet(metaclass=ABCMeta):
 
 
 class PWM(DataSet):
-    def make_data(self):
-        pass
+    def __init__(self, ratio: List[float] = None, period: int = 20, cycle: int = 5):
+        self.ratio = ratio
+        if self.ratio is None:
+            self.ratio = [0, 0.25, 0.5, 0.75, 1]
+        super().__init__(period * cycle, len(self.ratio))
+        self.period = period
+        self.cycle = cycle
 
-    def print_data(self):
-        pass
+    def single_data(self, factor) -> List:
+        data = []
+        scaling = 1000
+        for i in range(self.period):
+            if i < factor * self.period:
+                data.append((800+randrange(200))/scaling)
+            else:
+                data.append(randrange(200)/scaling)
+
+        return data
--- Model.py
+++ Model.py
@@ -1,13 +1,43 @@
 from abc import *
 from tensorflow import keras
 from tensorflow.keras.layers import LSTM, Dense
-from typing import List, Optional
+from typing import Optional
 from warnings import warn
 
 import Data
 
 
-class Model(metaclass=ABCMeta):
+class Model(keras.Sequential, metaclass=ABCMeta):
+    def __init__(self, data_set: Data, epoch: int, learning_rate: float):
+        super().__init__()
+        self.data_set: Data = data_set
+
+        self.epoch: int = epoch
+        self.learning_rate: float = learning_rate
+
+        self.data_path: str = ''
+        self.save_path: str = 'model'
+
+    @abstractmethod
+    def build_model(self):
+        pass
+
+
+class CategoricalLSTM(Model):
+    def __init__(self, data_set: Data, epoch: int, learning_rate: float, lstm_size: int):
+        super().__init__(data_set, epoch, learning_rate)
+        self.lstm_size = lstm_size
+
+    def build_model(self):
+        self.add(LSTM(self.lstm_size))
+        self.add(Dense(self.data_set.output_size, activation=keras.activations.softmax))
+
+        self.compile(loss=keras.losses.CategoricalCrossentropy(),
+                     optimizer=keras.optimizers.RMSprop(self.learning_rate),
+                     metrics=['mse', 'accuracy'])
+
+
+class ModelInit(metaclass=ABCMeta):
     def __init__(self, model: keras.Sequential, data_set: Data, epoch: int, learning_rate: float):
         self.model: Optional[keras.Sequential] = model
         self.data_set: Data = data_set
@@ -18,11 +48,8 @@ class Model(metaclass=ABCMeta):
         self.data_path: str = ''
         self.save_path: str = 'model'
 
-    def save_model(self):
-        self.model.save(self.save_path)
 
-
-class CategoricalLSTM(Model):
+class CategoricalLSTMInit(ModelInit):
     def __init__(self, data_set: Data, epoch: int, learning_rate: float, lstm_size: int):
         self.lstm_size = lstm_size
         m = keras.Sequential()
@@ -30,7 +57,7 @@ class CategoricalLSTM(Model):
         m.add(Dense(data_set.output_size, activation=keras.activations.softmax))
 
         m.compile(loss=keras.losses.CategoricalCrossentropy(),
-                  optimizer=keras.optimizers.RMSprop(super().learning_rate),
+                  optimizer=keras.optimizers.RMSprop(learning_rate),
                   metrics=['mse', 'accuracy'])
 
         super().__init__(m, data_set, epoch, learning_rate)
